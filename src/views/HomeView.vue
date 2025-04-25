bebb5cccdcc8e4f1b94dac1f5c4f221789410d74
danny_chen <danny_chen@seberon.com>
2025-04-25 14:14:58 +0800

    feat: 更新 Tailwind CSS 及相關套件版本並調整配置檔案 在 package.json 和 pnpm-lock.yaml 中新增 @tailwindcss/vite 和 @tailwindcss/postcss 套件，並將 tailwindcss 更新至版本 4.1.4。調整 postcss.config.js 以使用 @tailwindcss/postcss，並在 vite.config.js 中整合 tailwindcss 插件。此外，更新 main.css 檔案以使用 @import 語法引入 Tailwind CSS，並在 HomeView.vue 中新增背景和文字顏色樣式。

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,6 @@
 import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 <template>
-  <div>Home Page</div>
+  <div class="bg-black/20 text-red-500">Home Page</div>
   <HelloWorld />
 </template>
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import AutoImport from 'unplugin-auto-import/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -18,6 +19,7 @@ export default defineConfig({
         filepath: './.eslintrc-auto-import.mjs', // 設定自動生成的 ESLint 規則檔案路徑，預設為 `./.eslintrc-auto-import.json`
       },
     }),
+    tailwindcss(),
   ],
   server: {
     port: 3809,
